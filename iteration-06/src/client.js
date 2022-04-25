f6a0c192be717d2a1146877afa8a1f9f3174a0dd
Adam Slíva <xsliva@fi.muni.cz>
2022-04-25 19:19:25 +0200

    Delete client.js

--- iteration-06/src/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// IMPORTANT
-// Do NOT modify this file
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
